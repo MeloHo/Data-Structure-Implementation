374790e957f33ebef65647eac7738da3f1b4fa3f
MeloHo <yidongh@andrew.cmu.edu>
2021-01-05 23:42:47 -0800

    Fixed Bug in shared_ptr

--- shared_ptr_simple_implementation.cpp
+++ shared_ptr_simple_implementation.cpp
@@ -1,93 +1,81 @@
-#include <iostream>
+#include<iostream>
 
-template<class T>
+template<typename T>
 class shared_ptr
 {
 private:
   T* ptr;
-  int* count;
+  long* count;
   
-  inline void dec_count()
-  {
-    --(*count);
-  }
-  
-  inline void inc_count()
+  void increment_count()
   {
     ++(*count);
   }
   
-public:
-  shared_ptr(): ptr(nullptr), count(nullptr) {}
-  shared_ptr(T* incoming): ptr(incoming)
+  void decrement_count()
   {
-    count = new int(1);
+    --(*count);
+  }
+  
+public:
+  shared_ptr(): ptr(nullptr), count(nullptr){}
+  
+  explicit shared_ptr(T* incoming): ptr(incoming) {
+    count = new long(1);
+  }
+  
+  shared_ptr(const shared_ptr& incoming): ptr(incoming.ptr), count(incoming.count) {
+    increment_count();
+  }
+  
+  shared_ptr& operator=(const shared_ptr& incoming) {
+    if (ptr != nullptr) {
+      decrement_count();
+      if ((*count) == 0) {
+        delete ptr;
+        delete count;
+      }
+    }
+    
+    ptr = incoming.ptr;
+    count = incoming.count;
+    increment_count();
+  }
+  
+  long ref_count() const {
+    return *count;
   }
   
   ~shared_ptr()
   {
-    if (count != nullptr)
+    if (ptr)
     {
-      dec_count();
-      if (*count == 0)
-      {
-        delete ptr;
-        delete count;
-      }
-    }
-  }
-  
-  shared_ptr(const shared_ptr& incoming): ptr(incoming.ptr), count(incoming.count)
-  {
-    inc_count();
-  }
-  
-  shared_ptr& operator=(const shared_ptr& rhs)
-  {
-    if (count != nullptr)
-    {
-      dec_count();
-      if (*count == 0)
-      {
+      decrement_count();
+      if ((*count) == 0) {
         delete ptr;
         delete count;
       }
     }
     
-    ptr = rhs.ptr;
-    count = rhs.count;
-    inc_count();
-    
-    return *this;
   }
   
-  T& operator->()
-  {
-    return *ptr;
-  }
-  
-  T* operator*()
+  T* operator->()
   {
     return ptr;
   }
   
-  int ref_count() const
+  T& operator*()
   {
-    return *count;
+    return *ptr;
   }
   
   void reset()
   {
-    if (count != nullptr)
-    {
-      dec_count();
-      if (*count == 0)
-      {
-        delete ptr;
-        delete count;
-      }
+    decrement_count();
+    if ((*count) == 0) {
+      delete ptr;
+      delete count;
     }
-    
     ptr = nullptr;
     count = nullptr;
   }
@@ -95,22 +83,36 @@ public:
 
 class A
 {
+public:
+  int a;
+  A(int a_): a(a_){}
+  void printA()
+  {
+    std::cout << "value:" << a << std::endl;
+  }
 };
 
 int main()
 {
-  shared_ptr<A> ptr_1(new A());
-  std::cout << ptr_1.ref_count() << std::endl;
+  shared_ptr<A> ptr_1(new A(1));
+  std::cout << "ref_count:" << ptr_1.ref_count() << std::endl;
   
   shared_ptr<A> ptr_2(ptr_1);
-  std::cout << ptr_1.ref_count() << std::endl;
+  std::cout << "ref_count:" << ptr_2.ref_count() << std::endl;
+  std::cout << "ref_count:" << ptr_1.ref_count() << std::endl;
+  
   
   {
     shared_ptr<A> ptr_3 = ptr_1;
-    std::cout << ptr_3.ref_count() << std::endl;
+    std::cout << "ref_count:" << ptr_3.ref_count() << std::endl;
   }
   
-  std::cout << ptr_1.ref_count() << std::endl;
+  std::cout << "ref_count:" << ptr_1.ref_count() << std::endl;  
+  
+  (*ptr_1).printA();
+  ptr_1.reset();
+  
+  std::cout << "ref_count:" << ptr_2.ref_count() << std::endl;
   
   
   return 0;
